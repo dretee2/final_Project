26857acbc5e926f4a793c86a42e8e490e4d42391
Anthony <uyahanthony008@gmail.com>
2025-06-24 10:56:15 +0100

    New one on the user management

--- test_Services/test_userManagement.py
+++ test_Services/test_userManagement.py
@@ -8,6 +8,7 @@ token = get_access_token()
 os.makedirs("reports", exist_ok=True)
 
 user_management_schema= ReadSchemaProperties.get_user_management_schema()
+runner = SchemathesisRunner(token)
 
 for name, url in user_management_schema.items():
-    SchemathesisRunner.run_test(name, url, token)
+   runner.run_test(name, url)
