860c628c246558b2a5d0a8a6fdd3fd41caabf70d
Anthony <uyahanthony008@gmail.com>
2025-06-24 15:09:48 +0100

    new

--- test_Services/test_bytebox.py
+++ test_Services/test_bytebox.py
@@ -1,10 +1,10 @@
 from Utilities.runner import SchemathesisRunner
 from Utilities.readSchemaUrl import ReadSchemaProperties
-from Utilities.user_auth import get_access_token
+from Utilities.user_auth import get_access_token_for_user
 import os
 
 
-token = get_access_token()
+token = get_access_token_for_user()
 os.makedirs("reports", exist_ok=True)
 
 bytebox_schema= ReadSchemaProperties.get_bytebox_schema()
--- test_Services/test_userManagement.py
+++ test_Services/test_userManagement.py
@@ -1,10 +1,10 @@
 
 from Utilities.readSchemaUrl import ReadSchemaProperties
 from Utilities.runner import SchemathesisRunner
-from Utilities.user_auth import get_access_token
+from Utilities.user_auth import get_access_token_for_user
 import os
 
-token = get_access_token()
+token = get_access_token_for_user()
 os.makedirs("reports", exist_ok=True)
 
 user_management_schema= ReadSchemaProperties.get_user_management_schema()
