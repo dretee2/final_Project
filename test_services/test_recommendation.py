bb16a46a05b4ca04107f6ad48f7a7768fa85b60e
Anthony <uyahanthony008@gmail.com>
2025-07-05 22:52:07 +0100

    testing

--- test_services/test_recommendation.py
+++ test_services/test_recommendation.py
@@ -5,11 +5,12 @@ from Utilities.runner import SchemathesisRunner
 from Utilities.user_auth import get_access_token_for_user
 import os
 
-token = get_access_token_for_user()
-os.makedirs("reports", exist_ok=True)
+def test_recommendation():
+    token = get_access_token_for_user()
+    os.makedirs("reports", exist_ok=True)
 
-recommendation_schema= ReadSchemaProperties.get_recommendation_schema()
-runner = SchemathesisRunner(token)
+    recommendation_schema= ReadSchemaProperties.get_recommendation_schema()
+    runner = SchemathesisRunner(token)
 
-for name, url in recommendation_schema.items():
-    runner.run_test(name, url)
+    for name, url in recommendation_schema.items():
+        runner.run_test(name, url)
